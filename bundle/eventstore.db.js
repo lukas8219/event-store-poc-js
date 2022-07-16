061ddd4a1cda5199c7a50afd897c2899f1e51592
lucas <lucas.c4d@gmail.com>
2022-07-16 15:17:40 -0300

    [chore] removing bundle folder

--- bundle/eventstore.db.js
+++
@@ -1,7 +0,0 @@
-import { EventStoreDBClient } from "@eventstore/db-client";
-const EventStore = new EventStoreDBClient({
-    endpoint: 'esdb-node1:2113',
-}, {
-    insecure: true
-});
-export { EventStore };
